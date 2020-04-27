a63f2e3aaba8b671aff5fb418a4e3f99fe9372c2
mvuas10 <mvuas10@gmail.com>
2020-04-27 14:21:50 +0200

    Pokemon app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon/Pokemon.js";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: "90",
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+
+  {
+    name: "Bulbasaur",
+    weight: "6.9",
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+
+  {
+    name: "Mewtwo",
+    weight: "122",
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+
+  {
+    name: "Mega beedrill",
+    weight: "65",
+    awesome: true,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
+      <div className="page-content my-5">
+        <div className="test mx-5">
+          <main>
+            <Title content="Pokemon, I choose you!" />
+            <div className="mb-4"></div>
+            <div className="row">
+              {all_pokemon.map((pokemon) => (
+                <div className="col-md-6 col-lg-4">
+                  <Pokemon
+                    name={pokemon.name}
+                    weight={pokemon.weight}
+                    awesome={pokemon.awesome}
+                    terrifying={pokemon.terrifying}
+                    abilities={pokemon.abilities}
+                  />
+                </div>
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,10 +77,7 @@ function App() {
           Welcome to Codaisseur
         </a>
         <br></br>
-        <a>
-          How are you? Finally I am able to do something by myself
-        </a>
-
+        <a>Learn more</a>
       </header>
     </div>
   );
